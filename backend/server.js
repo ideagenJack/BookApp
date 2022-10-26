8babf185f5f9a9746b521893af121c6eb468d0ab
ideagenJack <jack.neville@ideagen.com>
2022-10-26 08:22:16 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const app = express(); 
+const port = process.env.PORT || 5000; 
+const database = require("./database/connect");
+const Book = require("./models/book")
+// This displays message that the server running and listening to specified port
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+  database.connect()
+  Book.sync()
+}
+)
+// create a GET route
+app.get('/express_backend', (req, res) => { 
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
